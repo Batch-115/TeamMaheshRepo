a8eb2c8a56069f46842a07bb77867d412748e2d3
maakonly4573 <maakonly4573@gmail.com>
2020-05-24 15:26:37 +0530

    Micro service structure 1st commit

---
+++ Loan_Disbursement_and_Ledger_Creation/src/main/java/com/webcore/app/easyemi/disbursement/model/DisbursementDetails.java
@@ -0,0 +1,12 @@
+package com.webcore.app.easyemi.disbursement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DisbursementDetails 
+{
+	
+	@Id
+	private long applicationID;
+}
